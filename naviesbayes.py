458ea6c0fa50ce05dae21082a939bea3842f962d
mtraino <47132377+mtraino@users.noreply.github.com>
2019-01-31 19:37:26 -0500

    Add files via upload

--- naviesbayes.py
+++ naviesbayes.py
@@ -1,51 +1,87 @@
+"""
+Matthew Traino
+Naive-Bayes classier
+Python 3.6.7
+"""
+# imports
 import numpy as np
+import matplotlib.pyplot as plt
 import ipdb
 from sklearn.metrics import confusion_matrix
 
-#loading the files needed
-traininglabels = np.loadtxt("traininglabels.txt", dtype=int)
-trainingdata = np.loadtxt("trainingdata.txt", dtype=int)
-testinglabels = np.loadtxt("testinglabels.txt", dtype=int)
-testingdata = np.loadtxt("testingdata.txt",dtype=int)
+# Load data from relevant files
+print("loading training data...")
+xtrain = np.loadtxt('trainingdata.txt', dtype=int)
+print("loading training labels...")
+ytrain = np.loadtxt('traininglabels.txt', dtype=int)
+print("loading testing data...")
+xtest = np.loadtxt('testingdata.txt', dtype=int)
+print("loading testing labels...")
+ytest = np.loadtxt('testinglabels.txt', dtype=int)
+print("loading newsgroups...")
+newsgroups = np.loadtxt('newsgroups.txt', dtype=str)
+print("loading vocabulary...")
+vocabulary = np.loadtxt('vocabulary.txt', dtype=str)
 
+# Change 1-indexing to 0
+xtrain[:, 0:2] -= 1
+ytrain -= 1
+xtest[:, 0:2] -= 1
+ytest -= 1
 
-maxword = int(testingdata.max())
-maxnum = int(traininglabels.max())
-Pclass = np.empty(maxnum)
-Pword = np.ndarray(shape=(maxword, maxnum))
+# Extract useful parameters
+num_training_documents = len(ytrain)
+num_testing_documents = len(ytest)
+num_words = len(vocabulary)
+num_newsgroups = len(newsgroups)
 
-for i in range(len(traininglabels)):
-    Pclass[traininglabels[i]-1] += 1
-Pclass = Pclass/len(traininglabels)
+print("\n=======================")
+print("TRAINING")
+print("=======================")
 
-for i in range(len(trainingdata)):
-    count = trainingdata[i, 2]
-    word = trainingdata[i, 1]-1
-    pos = traininglabels[trainingdata[i, 0]-1]-1
-    Pword[word, pos] += count
-Pword += .06 #Alpha value being added to the whole matrix
+# Estimate the prior probabilities
+print("Estimating prior probabilities via MLE...")
+priors = np.bincount(ytrain) / num_training_documents
 
-#sums up the colum
-for i in range(traininglabels.max()):
-    summ = sum(Pword[:, i])
-    Pword[:, i] = Pword[:, i]/(summ + (.06 * maxword))
+# Estimate the class conditional probabilities
+print("Estimating class conditional probabilities via MAP...")
+class_conditionals = np.zeros((num_words, num_newsgroups))
+rows = xtrain[:, 1].tolist()
+cols = ytrain[xtrain[:, 0]].tolist()
+np.add.at(class_conditionals, (rows, cols), xtrain[:, 2])
+alpha = (1 / num_words)
+class_conditionals += alpha
+class_conditionals /= np.sum(class_conditionals, 0)
 
-Pword = np.log(Pword)
-Pclass = np.log(Pclass)
+print("\n=======================")
+print("TESTING")
+print("=======================")
 
-#Product of P(Xj | Yi) get the P from training data and raises that P to the count in testing data
-PwordTesting = np.zeros((np.amax(testingdata[:, 0]), maxnum))
-for i in range(len(testingdata)):
-    print(i)
-    word = testingdata[i, 1]
-    for x in range(traininglabels.max()):
-        pvalue = Pword[(word-1), x]
-        pvalue *= testingdata[i, 2]
-        PwordTesting[testingdata[i, 0]-1, x] += pvalue
-PwordTesting += Pclass
+# Test the Naive Bayes classifier
+print("Applying natural log to prevent underflow...")
+log_priors = np.log(priors)
+log_class_conditionals = np.log(class_conditionals)
 
-a = np.argmax(PwordTesting, axis=1) + 1
-c = (testinglabels == a).mean()*100
-print("Accuracy: " + str(c))
-matrix = confusion_matrix(testinglabels, a)
-print(matrix)
+print("Counting words in each document...")
+counts = np.zeros((num_testing_documents, num_words))
+rows = xtest[:, 0].tolist()
+cols = xtest[:, 1].tolist()
+np.add.at(counts, (rows, cols), xtest[:, 2])
+
+print("Computing posterior probabilities...")
+log_posterior = np.matmul(counts, log_class_conditionals)
+log_posterior += log_priors
+
+print("Assigning predictions via argmax...")
+pred = np.argmax(log_posterior, 1)
+
+print("\n=======================")
+print("PERFORMANCE METRICS")
+print("=======================")
+
+# Compute performance metrics
+accuracy = np.mean(ytest == pred)
+print("Accuracy: {0:d}/{1:d} ({2:0.1f}%)".format(sum(ytest == pred), num_testing_documents, accuracy * 100))
+cm = confusion_matrix(ytest, pred)
+print("Confusion matrix:")
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in cm]))
